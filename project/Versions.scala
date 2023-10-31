f82fb5d743a43ee01de8a341032bc506d39684db
Hermann Hueck <hermann.hueck@mailbox.org>
2023-10-31 14:58:44 +0100

    bump versions

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,13 +4,13 @@ object Versions {
   val projectVersion = "0.1.0"
 
   // Scala
-  val scala2Version = "2.13.9"
+  val scala2Version = "2.13.12"
 
   // 3rd party libraries
-  val catsEffectVersion = "3.4.8"
+  val catsEffectVersion = "3.5.2"
   val catsRetryVersion  = "3.1.0"
   val munitVersion      = "0.7.29"
-  val slf4jVersion      = "2.0.6"
+  val slf4jVersion      = "2.0.9"
 
   // compiler plugins
   val kindProjectorVersion    = "0.13.2"
